227387cd53f1321e9128fd985c97e3deb1cbec8a
David Hong <hsurf22@gmail.com>
2018-05-11 17:44:52 -0700

    Features updated

--- src/components/Features/Features.vue
+++ src/components/Features/Features.vue
@@ -8,6 +8,57 @@
           <div class="page-container">
             <h2 class="large-title">Features</h2>
             <div class="middle-bar"></div>
+
+            <div class="feature-content-container">
+              <div class="feature-block">
+                <h6 class="feature-name">Client-side Encryption</h6>
+                <p class="article">
+                  An encrypted version of your private key will be stored
+                  in isolation on your device and MEWconnect will never
+                  expose your private key to any browser!
+                </p>
+              </div>
+              <div class="feature-block">
+                <h6 class="feature-name">Client-side Encryption</h6>
+                <p class="article">
+                  An encrypted version of your private key will be stored
+                  in isolation on your device and MEWconnect will never
+                  expose your private key to any browser!
+                </p>
+              </div>
+              <div class="feature-block">
+                <h6 class="feature-name">Client-side Encryption</h6>
+                <p class="article">
+                  An encrypted version of your private key will be stored
+                  in isolation on your device and MEWconnect will never
+                  expose your private key to any browser!
+                </p>
+              </div>
+              <div class="feature-block">
+                <h6 class="feature-name">Client-side Encryption</h6>
+                <p class="article">
+                  An encrypted version of your private key will be stored
+                  in isolation on your device and MEWconnect will never
+                  expose your private key to any browser!
+                </p>
+              </div>
+              <div class="feature-block">
+                <h6 class="feature-name">Client-side Encryption</h6>
+                <p class="article">
+                  An encrypted version of your private key will be stored
+                  in isolation on your device and MEWconnect will never
+                  expose your private key to any browser!
+                </p>
+              </div>
+              <div class="feature-block">
+                <h6 class="feature-name">Client-side Encryption</h6>
+                <p class="article">
+                  An encrypted version of your private key will be stored
+                  in isolation on your device and MEWconnect will never
+                  expose your private key to any browser!
+                </p>
+              </div>
+            </div>
           </div> <!-- .page-container -->
 
         </div> <!-- .rotate-child -->
