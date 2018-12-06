2f694439d4c89fe3138164f8546a82ddb6857141
mpolev364 <mpolev364@gmail.com>
2018-12-06 09:10:59 +0700

    Update tutorial link

--- src/views/Home/Components/UserGuide/UserGuide.vue
+++ src/views/Home/Components/UserGuide/UserGuide.vue
@@ -3,12 +3,12 @@
     <div class="container">
       <div class="row">
         <div class="col-lg-12">
-          <router-link to="/user-guide" class="user-guide-home__link">
+          <router-link to="/tutorial-videos" class="user-guide-home__link">
             <img src="~@/assets/images/user-guide.png" alt="" />
           </router-link>
           <div class="user-guide-home__text">
-            <h3>User guide</h3>
-            <p>This is our informational series dedicated to showing users all the workings of our MEWconnect smartphone app. <router-link to="/user-guide">Learn more.</router-link></p>
+            <h3>Tutorial videos</h3>
+            <p>This is our informational series dedicated to showing users all the workings of our MEWconnect smartphone app. <router-link to="/tutorial-videos">Learn more.</router-link></p>
           </div>
         </div>
       </div>
